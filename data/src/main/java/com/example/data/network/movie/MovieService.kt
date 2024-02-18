de3de7581e3c4552d66acef769d53d6470de43c0
yousefbeiruty <yousefbeiruty61@gmail.com>
2024-02-18 10:30:33 +0300

    Adding Compose Movie Details screen in compose.

--- data/src/main/java/com/example/data/network/movie/MovieService.kt
+++ data/src/main/java/com/example/data/network/movie/MovieService.kt
@@ -1,15 +1,10 @@
 package com.example.data.network.movie
 
 import com.example.data.network.common.NetworkResult
-import com.example.data.network.home.model.MostPopularResponse
-import com.example.data.network.movie.model.ResponseDto
 import com.example.data.network.services.ApiManager
 import com.example.navigationtutorial.model.MoviesListResponse
-import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.Query
-import java.util.Stack
 import javax.inject.Inject
+
 
 class MovieService @Inject constructor(private val apiManager: ApiManager) {
     companion object {
@@ -17,7 +12,6 @@ class MovieService @Inject constructor(private val apiManager: ApiManager) {
         private const val API_KEY = "api_key"
         const val SERVER_URL = "https://api.themoviedb.org/3/"
         private const val PATH_EVENT_DETAILS = "discover/movie"
-        const val MOVIE_API_KEY = "8f3845576311ddddc2ae7f7801641fdb"
     }
 
     suspend fun getMostPopular(key: String,page:Int,sortBy:String): NetworkResult<List<MoviesListResponse.Result>> {
